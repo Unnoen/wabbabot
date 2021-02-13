83c0f15c9c8b8bda665e19d7d37288a0164019dd
Your name <55751269+tr4wzified@users.noreply.github.com>
2021-02-13 01:15:29 +0100

    Work on setting up a modlist dev that owns a modlist

--- classes/ModlistAuthor.rb
+++
@@ -1,9 +0,0 @@
-require 'discordrb'
-require_relative '../helpers/typehelper.rb'
-class ModlistAuthor
-  def initialize(username, discord_id)
-    raise TypeError, 'modlist_author.initialize requires a Discord ID' unless only_digits? discord_id
-    @username = username
-    @id = discord_id
-  end
-end
